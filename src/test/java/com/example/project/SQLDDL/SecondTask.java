163529c83f64235f40369916b7bbce29ab4e42ce
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2022-10-05 10:37:14 +0300

    add new test for many to many relation exercise

--- src/test/java/com/example/project/SQLDDL/SecondTask.java
+++ src/test/java/com/example/project/SQLDDL/SecondTask.java
@@ -1,7 +1,6 @@
 package com.example.project.SQLDDL;
 
 import com.example.project.util.JdbcUtils;
-import org.assertj.core.api.AssertionsForInterfaceTypes;
 import org.junit.jupiter.api.*;
 
 import javax.sql.DataSource;
@@ -12,16 +11,18 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.project.util.JdbcUtils.fetchColumnValues;
 import static com.example.project.util.JdbcUtils.fetchTableNames;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Database should store information about employee, chats and its relations.
- * We already have two tables "employee" and "chat that have their own identifiers.
+ * We already have two tables "employee" and "chat" that have their own identifiers.
  * Create link table "employee_chats" that should have a composite key that consists of two foreign key columns.
  */
 public class SecondTask {
@@ -61,7 +62,7 @@ public class SecondTask {
 
 			List<String> columnTypes = fetchColumnValues(resultSet, "type_name");
 
-			AssertionsForInterfaceTypes.assertThat(columnTypes).containsExactlyInAnyOrder("BIGINT", "BIGINT");
+			assertThat(columnTypes).containsExactlyInAnyOrder("BIGINT", "BIGINT");
 		}
 	}
 
@@ -92,7 +93,7 @@ public class SecondTask {
 			resultSet.next();
 			String uniqueConstraintColumn = resultSet.getString("column_list");
 
-			assertThat(uniqueConstraintColumn).isEqualTo("employee_id,chat_id");
+			assertThat(uniqueConstraintColumn).isIn("employee_id,chat_id", "chat_id,employee_id");
 		}
 	}
 
@@ -121,8 +122,33 @@ public class SecondTask {
 					" WHERE table_name = 'employee_chat' AND constraint_type = 'REFERENTIAL' AND column_list = 'chat_id';");
 
 			boolean resultIsNotEmpty = resultSet.next();
-
 			assertTrue(resultIsNotEmpty);
+		}
+	}
+
+	@Test
+	@Order(7)
+	@DisplayName("The table have all relations for proper column in the other tables")
+	void checkInsertIntoTable() throws SQLException {
+		try (Connection connection = dataSource.getConnection()) {
+			Statement statement = connection.createStatement();
+			statement.execute("""
+							INSERT INTO CHAT values (1, 'programmer_chat');
+										INSERT INTO employee values (1, 'Vladislav', 'Tereshuk');
+										INSERT INTO employee_chat values (1, 1);
+					""");
+			ResultSet resultSet = statement.executeQuery("""
+					SELECT * FROM employee e
+					JOIN employee_chat ec on e.id = ec.employee_id
+					JOIN chat c on ec.chat_id = c.id;
+					""");
+			boolean resultIsNotEmpty = resultSet.next();
+			assertTrue(resultIsNotEmpty);
+			List<String> stringColumns = new ArrayList<>();
+			stringColumns.add(resultSet.getString("firstname"));
+			stringColumns.add(resultSet.getString("lastname"));
+			stringColumns.add(resultSet.getString("name"));
+			assertThat(stringColumns).contains("Vladislav", "Tereshuk", "programmer_chat");
 		}
 	}
 
